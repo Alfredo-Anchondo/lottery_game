f582dd3eecd41a97aff5c91c781aa4fb0601e8c4
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-02-29 16:29:01 -0600

    asd

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
     belongs_to :team
     belongs_to :team2, class_name: "Team", foreign_key: "team2_id"
     has_many :lotteries
+    has_one :sport_category
     
       def select_display
          team.name + " vs " + team2.name
