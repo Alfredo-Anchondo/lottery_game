5ab14256e60e39c22263362ce4ef606263617fc0
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-01 17:13:39 -0600

    FD

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,5 +1,12 @@
 class Lottery < ActiveRecord::Base
-  belongs_to :game 
+  belongs_to :game
+    before_update :update_winner, :if => :winner_number_changed? 
+    
+    
+    def update_winner
+        logger.info "ENTREEEEEEEEEEEEEEEEEE"
+    end    
+    
       def select_display
           game.team.name + " vs " + game.team2.name
       end
