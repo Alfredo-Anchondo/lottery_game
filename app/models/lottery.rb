7165a53dc10c33af03f474c84da8e8ffcf26c419
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-01 17:01:42 -0600

    sad

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,16 +1,7 @@
 class Lottery < ActiveRecord::Base
-  belongs_to :game
-  after_update :notify_systtem_if_winner_is_changed  
+  belongs_to :game 
       def select_display
           game.team.name + " vs " + game.team2.name
       end
-
-   
-    
-     
-    
-    def notify_systtem_if_winner_is_changed
-        notify_system if previous_changes['winner_number'].any?
-  end
     
 end
