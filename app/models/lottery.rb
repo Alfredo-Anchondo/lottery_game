bb7fc9b5afcf36d0aee8a67f4cddbcf6116dc739
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-02 09:40:53 -0600

    df

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -6,7 +6,7 @@ class Lottery < ActiveRecord::Base
     
     def update_winner
         logger.info "ENTREEEEEEEEEEEEEEEEEE"
-        logger.info game.user_lottery
+        logger.info user_lotteries
     end    
     
       def select_display
