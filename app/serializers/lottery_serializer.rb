6445daafef32fdc9b7cf69e24590f78c34004bb6
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-04 13:54:48 -0600

    nashj

--- app/serializers/lottery_serializer.rb
+++ app/serializers/lottery_serializer.rb
@@ -1,5 +1,6 @@
 class LotterySerializer < ActiveModel::Serializer
     attributes :id,:initial_balance, :rules, :description, :game_id, :winner_number, :initial_number, :final_number, :price
     
+    has_one :game
     
 end
