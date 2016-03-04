d4a386ef378832abbb410b82892f47fa90aa4617
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-04 11:48:22 -0600

    ghasd

--- app/models/user_lottery.rb
+++ app/models/user_lottery.rb
@@ -1,6 +1,8 @@
 class UserLottery < ActiveRecord::Base
   belongs_to :user
-  belongs_to :lottery    
+  belongs_to :lottery   
+    has_one :user
+    has_one :lottery
     
       def select_display
       name
