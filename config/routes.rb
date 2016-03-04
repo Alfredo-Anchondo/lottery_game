bc07069d345712b78006738255400c0222cdd44c
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-04 13:39:03 -0600

    hjasd

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
   resources :user_lotteries do
     collection do
         get 'winners'
-        get 'user_lotteries/winners_view'
     end
   end
         
@@ -39,6 +38,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   get 'welcome/index_client'  
+  get 'user_lotteries/winners_view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
