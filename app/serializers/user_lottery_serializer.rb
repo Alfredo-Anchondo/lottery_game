1408948d27521edea80a2b96c7b24e441d27a46b
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-04 11:43:24 -0600

    hsad

--- app/serializers/user_lottery_serializer.rb
+++ app/serializers/user_lottery_serializer.rb
@@ -1,3 +1,6 @@
 class UserLotterySerializer < ActiveModel::Serializer
-  attributes :id
+    attributes :id, :user_id, :lottery_id, :status, :ticket_number, :purchase_date
+    
+    has_one :user
+    has_one :lottery
 end
