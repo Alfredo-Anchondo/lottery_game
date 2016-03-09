58e74d2a1cf09227193b2a4e2ed5e2d98ee0f8f0
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-09 17:11:21 -0600

    ghsad
    Signed-off-by: Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     
     def lotteries
       @user = User.find(params[:id])
-        respond_with[@user.user_lotteries, Game.all]
+        respond_with(@user.user_lotteries.lottery.game, )
     end
 
   # GET /users/new
