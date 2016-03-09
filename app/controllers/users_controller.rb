94112108a53ed7b555ca8fb440e8fdc481ba20f2
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-09 17:20:56 -0600

    hjasd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,7 @@ class UsersController < ApplicationController
     
     def lotteries
       @user = User.find(params[:id])
-        @user_lottery = UserLottery.find(params[:id])
-        respond_with(@user.user_lotteries, @user_lottery.lottery.games )
+        respond_with(@user.user_lotteries)
     end
 
   # GET /users/new
