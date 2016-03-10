e5540a1c2c4f4aad8e0b051abf29f582e7822ac4
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-10 10:23:41 -0600

    hgsad

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -2,6 +2,8 @@ class Lottery < ActiveRecord::Base
   belongs_to :game
   before_update :update_winner, :if => :winner_number_changed? 
   has_many :user_lotteries 
+    has_one :team
+    has_one :team2
     
     
     def update_winner
