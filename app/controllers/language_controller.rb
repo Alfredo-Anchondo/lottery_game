c633a03c96e10b82bb61b6cd6fd6b384ba7de680
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-03-22 17:13:34 -0600

    hasd

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,5 +1,9 @@
- def i18n
+class LanguageController < ApplicationController 
+
+def i18n
     return if params[:locale].blank?
     I18n.locale = params[:locale]
     redirect_to :back
   end
+    
+end
