dba0cd1e6be8d107c9d479310487094655c7a331
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-06 14:08:22 -0500

    bhsd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+ 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
     
@@ -92,3 +92,5 @@ Rails.application.configure do
    
     
 end
+
+Rails.application.routes.default_url_options[:host] = 'donbillete.com'
