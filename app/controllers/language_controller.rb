bc3ecf5b57192ad5f041cb84242ada55b59d684d
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-11 12:54:19 -0500

    hjasd

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -4,18 +4,15 @@ class LanguageController < ApplicationController
     
   def i18n
     I18n.locale = search_profile
-      if :back
-    redirect_to :back
-      else
      redirect_to :root
-      end
+      
   end
     
 
     private def search_profile
         @user = current_user
         logger.info @user.language
-        if @user.language
+        if @user.language != "" || nil
             return @user.language
         else    
             return params[:locale]
