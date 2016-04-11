2c1015fdc12fc8c16a750c1e28a8ddfa5a1b59e6
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-11 13:42:48 -0500

    hjasd

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,5 +1,4 @@
 class LanguageController < ApplicationController 
-
     
   def i18n
     I18n.locale = search_profile
@@ -12,13 +11,17 @@ class LanguageController < ApplicationController
 
     private def search_profile
         @user = current_user
+        if @user
         if @user.language != "" || nil
             logger.info "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&Si entro $$$$$$$$$$$$$$$$$$$$$$$"
             return @user.language
         else    
             logger.info "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Agarro default %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
             return params[:locale] 
-        end    
+        end  
+        else
+            return params[:locale] 
+        end
     end
     
 end
