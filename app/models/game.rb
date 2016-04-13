c8d8ca0df15a165986dde86f53bae0b2713d2e89
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-12 21:55:17 -0500

    asdj

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,7 +14,7 @@ class Game < ActiveRecord::Base
     end
     
     def self.close_lottery_buy
-      x = where('game_date >= ?', DateTime.now.to_formatted_s(:long_ordinal)).order(:game_date).first  
+        x = where('game_date >= ?', DateTime.now.to_formatted_s(:db)).order(:game_date).first  
         logger.info "%$#%$##%$#%$#%$ Ya corrio el proceso $@$@#!@$" + x
     end
     
