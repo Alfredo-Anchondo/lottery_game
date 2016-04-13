de67975ef6b71acd2634102454cbb007b6bff313
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-13 15:37:45 -0500

    asd

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,7 +14,7 @@ class Game < ActiveRecord::Base
     end
     
     def self.close_lottery_buy
-        x = where(game_date: (DateTime.now.change(:sec => 0).to_formatted_s(:db) )..(DateTime.now.change(:sec => 0) + 59.seconds).to_formatted_s(:db)).order(:game_date).first  
+        x = where(game_date: ((DateTime.now.change(:sec => 0) + 2.hours).to_formatted_s(:db) .. ((DateTime.now.change(:sec => 0) + 2.hours + 59.seconds).to_formatted_s(:db)).to_formatted_s(:db)).order(:game_date).first  
       
         logger.info "%$#%$##%$#%$#%$ Ya corrio el proceso $@$@#!@$" 
         logger.info x
