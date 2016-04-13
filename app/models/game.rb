3b082d8028aaef195c7a2bd672b2b31aae680f20
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-13 13:55:51 -0500

    jasd

--- app/models/game.rb
+++ app/models/game.rb
@@ -17,7 +17,7 @@ class Game < ActiveRecord::Base
         x = where('game_date = ?', DateTime.now.change(:sec => 0)).order(:game_date).first  
         logger.info "%$#%$##%$#%$#%$ Ya corrio el proceso $@$@#!@$" 
         logger.info x
-        logger.info DateTime.now.change(:sec => 0).to_formatted_s(:db)
+        logger.info DateTime.now.change(:sec => 0)
     end
     
     
