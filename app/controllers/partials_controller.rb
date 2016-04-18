9ecad6063a425fa180d480b0a8c9fe523ea5b417
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-04-18 12:40:44 -0500

    asd
    Signed-off-by: Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -41,6 +41,7 @@ end
     device_session_id: params[:deviceIdHiddenFieldName]
   }
   @response_hash = @cards.create(card_hash.to_hash, customer['id'])
+     render :back
      
  rescue OpenpayException => e
      @e = e
