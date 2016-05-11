f5c2e00acd1fb32eaef2b8564ca2815112da9cea
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-05-11 14:02:25 -0500

    s

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-    attributes :id ,:team_id, :game_date, :description, :local_score, :visit_score, :team2_id
+    attributes :id ,:team_id, :game_date, :description, :local_score, :visit_score, :team2_id, :lottery
     has_one :team
     has_one :team2
     has_many :lotteries
