13b33116d881934118c3f9bf4d3129564bd6313a
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-05-27 10:29:42 -0500

    sdf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	load_and_authorize_resource
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :data, only: [:index, :new, :create, :edit, :update]
 	before_action :authenticate_user!, except: 'search_reference'
