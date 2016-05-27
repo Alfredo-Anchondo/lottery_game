d44adf03b5b69999e9e8cdc97ad16b2634608e61
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-05-27 13:21:05 -0500

    sdf

--- app/models/game.rb
+++ app/models/game.rb
@@ -72,7 +72,7 @@ class Game < ActiveRecord::Base
     end
  
     def self.today_games
-		no_winners = Lottery.where("winner_number  <> ''").pluck(:id)
+		no_winners = Lottery.where.not(winner_number: '').pluck(:id)
 		
 		return  Game.find(no_winners)
     end
