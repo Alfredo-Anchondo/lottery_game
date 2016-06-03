e575962f26b3a3af1f03725b48d5d0479a686d1d
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-06-03 11:24:22 -0500

    sdf

--- app/controllers/quiniela_users_controller.rb
+++ app/controllers/quiniela_users_controller.rb
@@ -1,8 +1,8 @@
 class QuinielaUsersController < ApplicationController
 	load_and_authorize_resource except: [:new,:create]
-  before_action :authenticate_user!
-   before_action :set_quiniela_user, only: [:show, :edit, :update, :destroy]	
-	before_action "administartor"
+    before_action :authenticate_user!
+    before_action :set_quiniela_user, only: [:show, :edit, :update, :destroy]	
+	before_action "administartor" except:[:new, :create]
  
 
   respond_to :html
