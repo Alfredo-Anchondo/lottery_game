8bb6c9abb9e90a130ca6a955eb5e196bfa69401b
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-06-08 14:32:23 -0500

    sad

--- app/models/quiniela_user.rb
+++ app/models/quiniela_user.rb
@@ -9,11 +9,18 @@ class QuinielaUser < ActiveRecord::Base
 		@quinielas = where('purchase_date > ? AND purchase_date <= ?',date1,date2)
 		@quinielas_total = @quinielas.count
 		@total_sales = 0;
+		@dates =[]
+		@date_array =[]
 		@quinielas.each do |quiniela|
 			@total_sales = @total_sales + Integer(quiniela.quiniela.price)
+			@dates.push(quiniela.purchase_date.strftime('%Y-%m-%d'))
 		end
+		@dates = @dates.uniq{|x| x}
 		logger.info @total_sales
-		return [@quinielas_total,@total_sales]
+		@dates.each do |date|
+			@date_array.push({label:date, cuantity: @quinielas.where('purchase_date > ? AND purchase_date < ?',Date.parse(date),(Date.parse(date)+1)).count})
+		end
+		return {totals: [@quinielas_total,@total_sales], date_array: @date_array}
 	end
 	
 	 def self.winners
--- app/models/user_lottery.rb
+++ app/models/user_lottery.rb
@@ -9,12 +9,20 @@ class UserLottery < ActiveRecord::Base
 	def self.today_sales(date1, date2)
 		@lotteries = where('purchase_date > ? AND purchase_date <= ?',date1,date2)
 		@lotteries_total = @lotteries.count
+		@dates = []
 		@total_sales = 0;
+		@date_array =[]
 		@lotteries.each do |lottery|
 			@total_sales = @total_sales + lottery.lottery.price
+			@dates.push(lottery.purchase_date.strftime('%Y-%m-%d'))
 		end
-		logger.info @total_sales
-		return [@lotteries_total,@total_sales]
+		@dates = @dates.uniq{|x| x}
+		@dates.each do |date|
+			@date_array.push({label: date, cuantity: @lotteries.where('purchase_date > ? AND purchase_date < ?',Date.parse(date),(Date.parse(date)+1)).count})
+		end
+		logger.info @dates
+		logger.info @date_array
+		return {totals: [@lotteries_total,@total_sales], date_array: @date_array}
 	end
  
     def self.winners
