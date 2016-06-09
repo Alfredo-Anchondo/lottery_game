1f65d2e9e8d037012e2188e81615efa49883825f
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-06-09 17:01:08 -0500

    sd

--- app/models/user_lottery.rb
+++ app/models/user_lottery.rb
@@ -14,6 +14,8 @@ class UserLottery < ActiveRecord::Base
 		@dates = []
 		@total_sales = 0
 		@date_array =[]
+		@sales_array = []
+		@total = 0
 		@lotteries.each do |lottery|
 			@total_sales = @total_sales + lottery.lottery.price
 			@dates.push(lottery.purchase_date.strftime('%Y-%m-%d'))
@@ -22,11 +24,17 @@ class UserLottery < ActiveRecord::Base
 		@dates = @dates.uniq{|x| x}
 		@dates = @dates.sort
 		@dates.each do |date|
+			@total = 0;
+			sales_day = @lotteries.where('purchase_date > ? AND purchase_date < ?',date,(Date.parse(date)+1))
+			sales_day.each do |sale|
+				@total += Integer(sale.lottery.price)
+			end
 			@date_array.push({label: date, cuantity: @lotteries.where('purchase_date >= ? AND purchase_date <= ?',Date.parse(date),(Date.parse(date)+1)).count})
+			@sales_array.push({label:date, sales: @total })
 		end
 		logger.info @dates
 		logger.info @date_array
-		return {totals: [@lotteries_total,@total_sales], date_array: @date_array}
+		return {totals: [@lotteries_total,@total_sales], date_array: @date_array, total_sales: @sales_array}
 	end
  
     def self.winners
