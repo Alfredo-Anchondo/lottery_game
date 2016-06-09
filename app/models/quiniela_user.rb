f0476eba91652990376d026f1f2af20c8e252803
Alfredo Anchondo Porras <alfredo_anchondo@hotmail.com>
2016-06-09 13:03:27 -0500

    sdf

--- app/models/quiniela_user.rb
+++ app/models/quiniela_user.rb
@@ -6,11 +6,13 @@ class QuinielaUser < ActiveRecord::Base
 	
 	def self.today_sales(date1, date2)
 		
-		@quinielas = where('purchase_date BETWEEN ? AND  ?',date1,date2)
+		@quinielas = where(purchase_date: date1..date2)
 		@quinielas_total = @quinielas.count
 		@total_sales = 0
 		@dates =[]
 		@date_array =[]
+		@sales_array = []
+		@total = 0
 		@quinielas.each do |quiniela|
 			@total_sales = @total_sales + Integer(quiniela.quiniela.price)
 			@dates.push(quiniela.purchase_date.strftime('%Y-%m-%d'))
@@ -18,10 +20,16 @@ class QuinielaUser < ActiveRecord::Base
 		@dates = @dates.uniq{|x| x}
 		@dates = @dates.sort
 		logger.info @total_sales
-		@dates.each do |date|
-			@date_array.push({label:date, cuantity: @quinielas.where('purchase_date > ? AND purchase_date < ?',date,(Date.parse(date)+1)).count})
+	    @dates.each do |date|
+			@total = 0;
+			sales_day = @quinielas.where('purchase_date > ? AND purchase_date < ?',date,(Date.parse(date)+1))
+			sales_day.each do |sale|
+				@total += Integer(sale.quiniela.price)
+			end
+			@date_array.push({label:date, cuantity: @quinielas.where('purchase_date >= ? AND purchase_date <= ?',Date.parse(date),(Date.parse(date)+1)).count })
+			@sales_array.push({label:date, sales: @total })
 		end
-		return {totals: [@quinielas_total,@total_sales], date_array: @date_array}
+		return {totals: [@quinielas_total,@total_sales], date_array: @date_array, total_sales: @sales_array}
 	end
 	
 	 def self.winners
