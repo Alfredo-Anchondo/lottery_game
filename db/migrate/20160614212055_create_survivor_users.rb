4cd3ba5bf222d3d0355c33d120cc43773ec2f573
Techno Logic <jluna@publiwi.com>
2016-06-16 16:53:07 -0500

    Changes

--- db/migrate/20160614212055_create_survivor_users.rb
+++ db/migrate/20160614212055_create_survivor_users.rb
@@ -2,13 +2,13 @@ class CreateSurvivorUsers < ActiveRecord::Migration
   def change
     create_table :survivor_users do |t|
       t.references :survivor_week_game, index: true, null:false
-	  t.foreign_key :survivor_week_games	
-      t.references :team, index: true, null:false	
+	  t.foreign_key :survivor_week_games
+      t.references :team, index: true, null:false
 	  t.foreign_key :teams
       t.datetime :purchase_date, null:false
       t.references :user, index: true, null:false
 	  t.foreign_key :users
-      t.string :status, null:false
+      t.string :status, null:false, default: "bought"
 
       t.timestamps
     end
