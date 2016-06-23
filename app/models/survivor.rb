a56f5efb4f6f035d08a4d4340f5b616448000c9d
Techno Logic <jluna@publiwi.com>
2016-06-23 13:05:05 -0500

    Changes

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -20,7 +20,13 @@ class Survivor < ActiveRecord::Base
       total_winners = survivor_users.winner.count
 
       if total_winners > 0 && initial_balance > 0
-        profit = initial_balance.to_f / total_winners
+        if percentage.present? && survivor_users.winner.find_by(:user_id => user_id).present?
+          percentage_profit = initial_balance * percentage / 100
+          user.update(:balance => user.balance + percentage_profit)
+          profit = (initial_balance.to_f - percentage_profit) / total_winners
+        else
+          profit = initial_balance.to_f / total_winners
+        end
 
         survivor_users.winner.each do |s|
           s.user.update(:balance => s.user.balance + profit)
