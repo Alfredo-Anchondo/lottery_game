c017caf630a7a8efafcb2272f930cff7651439cb
Techno Logic <jluna@publiwi.com>
2016-06-30 13:49:07 -0500

    Changes

---
+++ test/models/survivor_user_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class SurvivorUserTest < ActiveSupport::TestCase
+  def setup
+
+  end
+
+  # test "Can't buy more tickets" do
+  #   survivor_week_survivor = survivor_week_survivors(:survivor_from_aanchondo_week_4)
+  #   team = teams(:nuggets)
+  #   user = users(:aanchondo)
+  #   survivor_user_parent = survivor_users(:survivor_user_1)
+  #   survivor_user = SurvivorUser.create(survivor_week_survivor: survivor_week_survivor, team: team, user: user, purchase_date: "2016-10-16", survivor_user: survivor_user_parent)
+  #   survivor_user.valid?
+  #   puts survivor_user.errors.full_messages
+  #   assert_not survivor_user.valid?
+  # end
+end
