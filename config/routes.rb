05817c229b0a090f38c440d86a34046160d5f329
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-08-29 12:36:15 -0500

    sdf

--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,7 @@ Rails.application.routes.draw do
     end
 
 
-    constraints subdomain: "webhooks" do
+    constraints subdomain: :webhooks do
         post '/webhookdemo' => 'webhooks#demo', as: :receive_webhooks
         get '/webhooks', to: 'webhooks#receive'
     end
