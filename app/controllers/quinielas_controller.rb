6d53f575ecda7109a49f73257ebc8b01574cef73
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-10-03 11:56:55 -0500

    asd

--- app/controllers/quinielas_controller.rb
+++ app/controllers/quinielas_controller.rb
@@ -3,18 +3,18 @@ class QuinielasController < ApplicationController
   before_action :authenticate_user!
   before_action :set_quiniela, only: [:show, :edit, :update, :destroy]
   respond_to :html
-  respond_to :json	
+  respond_to :json
 
-	
+
 	def quiniela_details
 		@quiniela = Quiniela.find(params[:quiniela_id])
 		@quiniela_users = QuinielaUser.where('quiniela_id = ?', params[:quiniela_id])
 	end
-	
+
 	def last_quinielas
 		render :json => Quiniela.last_quinielas
 	end
-	
+
 	def buy_random_quinielas
 		@numbers = params[:numbers]
 		@price = params[:price]
@@ -28,7 +28,7 @@ class QuinielasController < ApplicationController
 		if @purchase_gift == '' || @purchase_gift == "NaN" || @purchase_gift == nil
 			@purchase_gift = 0
 		end
-		
+
 		render nothing: true
 
 		if params[:normal_buy] != 'true'
@@ -38,28 +38,28 @@ class QuinielasController < ApplicationController
 		else
 			@user.update( {balance: @total_balance_update})
 		end
-		
+
 		@numbers.each_with_index do |_, i|
 			QuinielaUser.create({user_id: @user.id, quiniela_id: @tira, status: 'Comprado', ticket_number: @numbers[Integer(i)], purchase_date: DateTime.now})
 		end
 		#BuyMailer.buy_many_tickets(@user_id, @array_values, @lottery).deliver
 
 	end
-	
+
 	def get_quinielas
 		@quinielas = Quiniela.all
-		render :json => @quinielas		
+		render :json => @quinielas
 	end
-	
-	def get_quinielas_no_winner	
-		render :json => Quiniela.find_no_winners 		
+
+	def get_quinielas_no_winner
+		render :json => Quiniela.find_no_winners
 	end
-	
+
 	def toclose
 		render :json => Quiniela.toclose
 	end
-	
-	
+
+
 	def index
     	@quinielas = Quiniela.all
     	respond_with(@quinielas)
@@ -93,7 +93,7 @@ class QuinielasController < ApplicationController
     @quiniela.save
     respond_with(@quiniela)
   end
-	
+
 	def save_result
 		@quiniela = Quiniela.find(params[:id])
 		@result = params[:canvas]
@@ -122,8 +122,8 @@ class QuinielasController < ApplicationController
     end
 
     def quiniela_params
-		params.require(:quiniela).permit(:initial_balance, :price, :game_id, :description, :purchase_gift_tickets, :to_mainpage,  :winner_number, :show_banner, :cap_result, :last_questions, :last_questions_text, quiniela_questions_attributes: [:id, :question_id, :_destroy])
+		params.require(:quiniela).permit(:initial_balance, :price, :game_id, :description, :purchase_gift_tickets, :to_mainpage,  :winner_number,  :cap_result, :last_questions, :last_questions_text, quiniela_questions_attributes: [:id, :question_id, :_destroy])
     end
-	
-      
+
+
 end
