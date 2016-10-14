f00f253242361440286cff248b25735b7b02efef
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-10-14 15:02:28 -0500

    dsf

--- app/models/relation_tira_question.rb
+++ app/models/relation_tira_question.rb
@@ -1,4 +1,10 @@
 class RelationTiraQuestion < ActiveRecord::Base
   belongs_to :tira_enrachate
   belongs_to :question_enrachate
+
+
+  def question_date
+    question_enrachate.program_date
+  end
+
 end
