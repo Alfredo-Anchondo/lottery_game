26dc08e3657c3ce162880a527409bcad9feff351
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-10-17 14:52:40 -0500

    dfg

--- app/models/tira_enrachate.rb
+++ app/models/tira_enrachate.rb
@@ -3,31 +3,31 @@ class TiraEnrachate < ActiveRecord::Base
     has_many :relation_tira_questions
     has_many :question_enrachates, :through => :relation_tira_questions
     has_many :enrachates, :through => :relation_enrachate_tiras
-    
+
     after_create :create_relation_enrachate
-    
+
     def select_display
         return name
     end
-        
-    
+
+
     def create_relation_enrachate
         logger.info "entre en el ciclo"
-        enracha = Enrachate.where("initial_date < ? and end_date > ?",program_date,program_date)
-        
+        enracha = Enrachate.where("initial_date < ? and end_date > ? and winner IS NULL",program_date,program_date)
+
         enracha.each do |game|
           relation = RelationEnrachateTira.new
           relation.enrachate_id = game.id
           relation.tira_enrachate_id = id
             relation.save
         end
-     
+
     end
-        
-        
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
 end
