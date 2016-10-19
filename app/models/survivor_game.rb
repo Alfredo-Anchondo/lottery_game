e41049b13668b37f1e1ac343c88d8ce803afe2d2
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-10-19 13:24:15 -0500

    sdf

--- app/models/survivor_game.rb
+++ app/models/survivor_game.rb
@@ -493,9 +493,10 @@ class SurvivorGame < ActiveRecord::Base
 				    end
 
 
-                     current_tickets2 =  survivor_week_game.survivor_users
-                     logger.info "contador de boletos global " + current_tickets2.count.to_s 
+
                 Survivor.where('status = ?','Enjuego').each do |survivor|
+                  current_tickets2 =  survivor_week_game.survivor_users
+                  logger.info "contador de boletos global " + current_tickets2.count.to_s 
                    xsurvivor_week_survivor_current_id = SurvivorWeekSurvivor.where('survivor_id = ? AND survivor_week_game_id = ?',survivor.id, survivor_week_game.id ).first
                   current_tickets2 =  current_tickets2.where("survivor_week_survivor_id = ?",xsurvivor_week_survivor_current_id.id)
                   logger.info "contador de boletos " + current_tickets2.count.to_s
