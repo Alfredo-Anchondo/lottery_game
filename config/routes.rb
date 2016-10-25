e5c8ea6d62691831195740a0211eecb79b095959
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-10-25 16:49:20 -0500

    hgf
    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
 
   resources :tira_enrachates
 
-  resources :enrachates
+  resources :enrachates do
+    collection do
+      get "enrachate_stats"
+    end
+    end
+  end
 
   resources :pick_user_games
 
