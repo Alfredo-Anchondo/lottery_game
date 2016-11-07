4123cb16d0f94716ccdd2bf7c42b0b0413990edd
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-11-07 12:31:33 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,7 +8,22 @@ class WelcomeController < ApplicationController
        @picks = Pick.where(:to_mainpage => true)
 			 @enrachates = Enrachate.where("initial_date >= ? and end_date >= ? and winner IS NULL",DateTime.now.to_date,DateTime.now.to_date)
 			 @enrachate = Enrachate.where("type_enrachate = ? and end_date > ? and initial_date < ? and winner IS ?",0,Time.now,Time.now, nil).first
-  end
+
+			 @current_tira = @enrachate.current_tira
+			 @last_tira = @enrachate.past_tira
+			 @future_tira = @enrachate.future_tira
+			 @tickets_for_enrachate = EnrachateUser.where("enrachates_id = ?", @enrachate.id).pluck(:enrachate_user_id).uniq
+			 @racha_values = []
+				@tickets_for_enrachate.each do | ticket |
+					@tickets_s = EnrachateUser.where("enrachate_user_id = ? and status = ?",ticket, "alive")
+					@count_ticket = EnrachateUser.where("enrachate_user_id = ? and status = ?",ticket, "alive").count
+					last =  @tickets_s.where("tira_enrachate_id = ?", @last_tira.id).count
+					current =  @tickets_s.where("tira_enrachate_id = ?", @current_tira.id).count
+					if last != 0 || current != 0
+					@racha_values.push(@count_ticket)
+					end
+				end
+	end
 
 	def no_sign_in
 		render "welcome/terms"
