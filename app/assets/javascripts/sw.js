1cbb568a73fcbf38113a9bcf2ececd4978f71a8c
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-11-11 13:28:35 -0600

    sdf

--- app/assets/javascripts/sw.js
+++ app/assets/javascripts/sw.js
@@ -1,13 +1,1 @@
-function onPush(event) {
-  var title = (event.data && event.data.text()) || "Yay a message" ;
 
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: "We have received a push message",
-      icon: "/assets/nombre.png",
-      tag:  "push-simple-demo-notification-tag"
-    });
-  )
-}
-
-self.addEventListener("push", onPush);
