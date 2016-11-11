ab9d15139071cbdd31ac7b14334c37d1dc895ac6
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-11-11 14:11:06 -0600

    dsf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,19 +38,6 @@
 //= require jquery.innerfade
 //= require serviceworker-companion
 
-if ('serviceWorker' in navigator) {
-  console.log('Service Worker is supported');
-  navigator.serviceWorker.register('/assets/sw.js')
-    .then(function(registration) {
-      console.log('Successfully registered!', ':^)', registration);
-      registration.pushManager.subscribe({ userVisibleOnly: true })
-        .then(function(subscription) {
-            console.log('endpoint:', subscription.endpoint);
-        });
-  }).catch(function(error) {
-    console.log('Registration failed', ':^(', error);
-  });
-}
 
 
 
