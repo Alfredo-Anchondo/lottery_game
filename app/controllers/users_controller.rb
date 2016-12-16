40888e6119aac7ce3df8567dc4d891c248b0125e
alfredo anchondo <alfredo_anchondo@hotmail.com>
2016-12-16 14:05:21 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ end
 
 	def send_mails_all
 		logger.info "Entre en el primer metodo"
-		User.send_mails_all(params[:emails], params[:content], params[:subject])
+		User.send_mails_all(params[:mails], params[:content], params[:subject])
 		render json: true
 	end
 
