367519d04cf38a1efcc79ffd7c248dd91425829f
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-18 12:05:17 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module LotteryGame
 	    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any,:expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'], :methods => [:get, :post, :options]
+        resource '*', :headers => :any,:expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],  :methods => [:get, :post, :options, :delete, :put]
       end
     end
 
