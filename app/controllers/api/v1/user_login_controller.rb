c1fb464e82fdb3cad22501130301ccb29b125000
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-23 12:47:14 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/controllers/api/v1/user_login_controller.rb
+++ app/controllers/api/v1/user_login_controller.rb
@@ -6,8 +6,24 @@ before_action :authenticate_user!
   end
 
   def buy_lottery
-    UserLottery.create(:user_id => params[:user_id], :lottery_id => params[:lottery_id], :status => "Comprado", :ticket_number => params[:ticket_number], :purchase_date => Time.now)
-    render json: true
+    @lottery = Lottery.find(params[:lottery_id])
+    @user = User.find(params[:user_id])
+    if(@user.gift_credit.to_i >= @lottery.price.to_i)
+      @user.update(:gift_credit => @user.gift_credit.to_i - @lottery.price.to_i)
+      @lottery.update(:initial_balance => (@lottery.price * 0.95) + @lottery.initial_balance )
+      UserLottery.create(:user_id => @user.id, :lottery_id => @lottery.id, :status => "Comprado", :ticket_number => params[:ticket_number], :purchase_date => Time.now)
+          render json: true
+    else
+      if(@user.balance.to_f >= @lottery.price.to_i)
+        @user.update(:balance => @user.balance.to_f - @lottery.price.to_i)
+        @lottery.update(:initial_balance => (@lottery.price * 0.95) + @lottery.initial_balance )
+        UserLottery.create(:user_id => @user.id, :lottery_id => @lottery.id, :status => "Comprado", :ticket_number => params[:ticket_number], :purchase_date => Time.now)
+            render json: true
+      else
+        render json: false
+      end
+    end
+
   end
 
 
