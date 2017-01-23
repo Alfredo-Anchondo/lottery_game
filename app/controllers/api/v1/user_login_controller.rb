332d94750fab8b9e55460563bd9ec234dd4140b4
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-23 12:24:27 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/controllers/api/v1/user_login_controller.rb
+++ app/controllers/api/v1/user_login_controller.rb
@@ -4,4 +4,11 @@ before_action :authenticate_user!
   def next_game
       render :json => Game.next_game
   end
+
+  def buy_lottery
+    UserLottery.create(:user_id => params[:user_id], :lottery_id => params[:lottery_id], :status => "Comprado", :ticket_number => params[:ticket_number], :purchase_date => Time.now)
+    return true
+  end
+
+
 end
