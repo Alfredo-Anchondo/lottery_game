b1580d1312279718fc4baea37168ab785cac5b23
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-24 10:29:18 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/serializers/enrachate_serializer.rb
+++ app/serializers/enrachate_serializer.rb
@@ -1,6 +1,6 @@
 class EnrachateSerializer < ActiveModel::Serializer
-  attributes :id, :price, :initial_balance, :percentage, :type_enrachate, :description, :winner, :initial_date, :end_date
+  attributes :id, :price, :initial_balance,:name, :percentage, :type_enrachate, :description, :winner, :initial_date, :end_date
   has_many :relation_enrachate_tiras
 
-    
+
 end
