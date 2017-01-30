fc130e1aedcd52d412c1f0e8935b30ef1747f4cb
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-30 13:52:42 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/serializers/enrachate_user_serializer.rb
+++ app/serializers/enrachate_user_serializer.rb
@@ -1,6 +1,8 @@
 class EnrachateUserSerializer < ActiveModel::Serializer
   attributes :id, :answer, :status, :purchase_date, :enrachate_user_id
+
   has_one :question_enrachate
   has_one :tira_enrachate
+  has_one :enrachate ,  :through => :tira_enrachate
   has_one :user
 end
