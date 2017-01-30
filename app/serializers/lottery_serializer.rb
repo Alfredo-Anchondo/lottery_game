077e3a796921775f8d0b65995e1cb157a9896ba3
alfredo anchondo <alfredo_anchondo@hotmail.com>
2017-01-30 13:01:42 -0600

    Signed-off-by: alfredo anchondo <alfredo_anchondo@hotmail.com>

--- app/serializers/lottery_serializer.rb
+++ app/serializers/lottery_serializer.rb
@@ -1,6 +1,6 @@
 class LotterySerializer < ActiveModel::Serializer
 
-    attributes :id,:initial_balance, :rules, :description, :game2_id, :game_id, :winner_number, :initial_number, :final_number, :price, :purchase_gift_tickets, :total_sales, :to_mainpage
+    attributes :id,:initial_balance, :rules, :description, :game2_id, :game_id, :winner_number, :initial_number, :final_number, :price, :purchase_gift_tickets, :select_display, :total_sales, :to_mainpage
 
 
     has_one :game2
